8e9c6cebbf29023e77f064e9767ef40fce44ae21
Anna Vovchenko <coolahhy@gmail.com>
2018-03-25 23:49:20 +0300

    added economy block to shopping cart

--- shopping-cart.php
+++ shopping-cart.php
@@ -127,30 +127,7 @@
 		</a>
     </section>
 
-    <section class="economy">
-        <div class="economy-container wrapper">
-            <h2 class="economy-title">Экономьте при покупке набора</h2>
-            <div class="economy-items">
-                <div class="economy-item">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/products/standart-set.png" alt="Комплект "Стандартный"">
-                    <p class="economy-item-title">Комплект 	&laquo;Стандартный&raquo;</p>
-                </div>
-                <span class="math">+</span>
-                <div class="economy-item">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/products/stand-set.png" alt="Автоматика Miller">
-                    <p class="economy-item-title">Автоматика Miller</p>
-                </div>
-                <span class="math">=</span>
-                <div class="economy_deal">
-                    <p class="economy-prices">
-                        <span class="economy-last-price">3500 грн</span>
-                        <span class="economy-price"><b>2990</b> грн</span>
-                    </p>
-                    <button class="btn">Добавить в корзину</button>
-                </div>
-            </div>
-        </div>  
-    </section>	
+   <?php get_template_part('economy'); ?>
     </main>
 
 
